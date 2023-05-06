c499cb3c08f4fd0c748388ad1a93f6435a6bbe39
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-05-06 10:14:02 -0300

    feat: url config, models and communication with endpoints from the backend

---
+++ src/app/config/api_config.ts
@@ -0,0 +1,4 @@
+export const API_CONFIG = {
+    productUrl : 'http://localhost:8080/api/v1/product',
+    customerUrl : 'http://localhost:8080/api/v1/customer'
+}
---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { API_CONFIG } from '../config/api_config';
+import { Customer } from '../models/customer';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor(private http: HttpClient) { }
+
+  findById(idCustomer: any): Observable<Customer> {
+    return this.http.get<Customer>(`${API_CONFIG.customerUrl}/findProduct/${idCustomer}`);
+  }
+
+  findAll(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(`${API_CONFIG.customerUrl}/list`);
+  }
+
+  create(customer: Customer): Observable<Customer[]> {
+    return this.http.post<Customer[]>(`${API_CONFIG.customerUrl}/create`, customer);
+  }
+
+  update(customer: Customer): Observable<Customer[]> {
+    return this.http.put<Customer[]>(`${API_CONFIG.customerUrl}/update`, customer);
+  }
+
+  delete(idCustomer: any): Observable<Customer> {
+    return this.http.delete<Customer>(`${API_CONFIG.customerUrl}/delete/${idCustomer}`);
+  }
+
+}
