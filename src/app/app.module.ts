053d631ca42e93efba898ad82a8c528b9510d3bc
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-05-05 22:35:14 -0300

    feat: components created and configuration of the basic route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
+//Default Imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//Imports for Components
+import { BodyComponent } from './components/body/body.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+//Immports for Screens
+import { HomeComponent } from './screens/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    SidenavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
